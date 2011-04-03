167eac0890c66fef7eb30baa9fac909035835246
Michiel Van Vlaardingen <michiel@unknown-44-2a-60-f3-0e-12.lan>
2011-04-03 16:07:28 +0200

    First Comit

---
+++ source/consyn.graph.js
@@ -0,0 +1,53 @@
+/*
+ * Raphael Based Graphing Library
+ *
+ *
+ */
+ 
+
+var ConsynGraph = (function(){
+    var extend = function(a,b){
+      for(var i in b){
+        a[i] = b[i]; 
+      }
+    }
+        
+    var view_func = function ConsynGraph_View(series, opts){
+      this.series = series || [];
+      this.opts = extend({
+        
+      }, opts);
+    }
+    view_func.prototype = {
+      add: function(s){
+        this.series.push(s);
+      },
+      draw: function(el){
+        this.paper = Raphael(el);
+      },
+      redraw: function(){
+        alert("TODO: implement"); 
+      }
+      
+      
+    };
+    
+    var serie_func = function ConsynGraph_Serie(data, opts){
+      this.data = data || [];
+      this.opts = extend({
+          
+      }, opts);
+    }
+    serie_func.prototype = {
+      draw: function(){
+        
+      }
+    };
+    
+    
+    return {
+      View: view_func,
+      Serie: serie_func,
+      
+    };
+})();
